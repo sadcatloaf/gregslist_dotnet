94cb47a9734b7f1010d85e8f1722fbe0cc4c7f05
Jeremy Fowler <jembyfowler@gmail.com>
2025-01-15 11:09:04 -0700

    fancy new mapping types

--- Repositories/CarsRepository.cs
+++ Repositories/CarsRepository.cs
@@ -19,7 +19,7 @@ public class CarsRepository
     FROM cars 
     JOIN accounts ON cars.creator_id = accounts.id;";
 
-    List<Car> cars = _db.Query<Car, Account, Car>(sql, (car, account) =>
+    List<Car> cars = _db.Query(sql, (Car car, Account account) =>
     {
       car.Creator = account;
       return car;
